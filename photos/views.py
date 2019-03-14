d5e947e75ce15a2937764e77f96d351f819d4eec
Hyacinthe04 <hyacinthen04@gmail.com>
2019-03-14 17:50:13 +0200

    first commit

---
+++ photos/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http  import HttpResponse
+
+
+# Create your views here.
+def welcome(request):
+    return render(request, 'all-photos/welcome.html')
+
+
+
+
