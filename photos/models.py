b11b5a376ad6c9d244606b69a23829851cb742ee
Hyacinthe04 <hyacinthen04@gmail.com>
2019-03-14 18:30:50 +0200

    add models in models.py file

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    Image = models.CharField(max_length =30)
+    name = models.CharField(max_length =30)
+    description = models.CharField(max_length =30)
+    location = models.ForeignKey(location)
+    category = models.ForeignKey(category)
+
+
+
