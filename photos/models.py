73f2ab40ea7f8870bbb1b47cb4af7c62426de987
Hyacinthe04 <mnuriarth28@gmail.com>
2019-03-15 08:50:11 +0200

    define location and category in models file

--- photos/models.py
+++ photos/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    category = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
     Image = models.CharField(max_length =30)
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =30)
-    location = models.ForeignKey(location)
-    category = models.ForeignKey(category)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.name
 
 
 
