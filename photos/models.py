6e827b981e1119eee8825db9504b7314b8731d47
Hyacinthe04 <mnuriarth28@gmail.com>
2019-03-15 09:12:54 +0200

    put the methods in models.py

--- photos/models.py
+++ photos/models.py
@@ -7,11 +7,30 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def save_location(self):
+        self.save()    
+
+    def update_location(self):
+        self.update()        
+
+    def delete_location(self):
+        self.delete()    
+
 class Category(models.Model):
     category = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()    
+
+    def update_category(self):
+        self.update()        
+
+    def delete_category(self):
+        self.delete()    
+
 
 class Image(models.Model):
     Image = models.CharField(max_length =30)
@@ -22,6 +41,26 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_image(self):
+        self.save()    
+
+    def update_image(self):
+        self.update()        
+
+    def delete_image(self):
+        self.delete()  
+
+    def get_image_by_id(id):
+        self.get_image_by_id()    
+
+    def search_image(category):
+        self.search_image()        
+
+    def filter_by_location(location):
+        self.filter_by_location()    
+     
+
 
 
 
